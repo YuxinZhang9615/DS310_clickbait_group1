64d4834ce2faa721f48ba82ef563fa990b52e16d
YuxinZhang9615 <YuxinZhang9615@gmail.com>
2017-09-18 17:01:44 -0400

    first commit from Diana(Yuxin)

---
+++ clickbait.R
@@ -0,0 +1,7 @@
+###Project 1: Clickbait
+###Team: Group 3
+###Team Member: Yuxin Zhang, Rafael Vinluan, Nicholas Cianci, Cameron Zenier
+###Date: 9/18/2017
+###Due: 10/8/2017
+###############################################################################
+
